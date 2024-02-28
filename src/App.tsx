cc01c746261c440b8b6d1e1b0eca730d8ccc7f5b
Gonçalo Ramalho <goncalo_ramalho@hotmail.com>
2024-02-28 14:53:23 +0000

    Create header base and add actoin color to tailwind theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import './App.css';
+import Header from '@components/Header';
 
 function App() {
   return (
     <>
-      <h1>Margarida & Hugo</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum alias
-        sequi natus tempora adipisci eum hic dicta, illum numquam quo doloremque
-        fugiat odio repellat accusantium? Exercitationem facilis aliquid quo
-        debitis?
-      </p>
+      <Header />
     </>
   );
 }
