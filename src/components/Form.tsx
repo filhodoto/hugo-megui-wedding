4b65082edb4d0f4bdd039ce978d7320477ea1bae
Gonçalo Ramalho <goncalo_ramalho@hotmail.com>
2024-02-29 15:49:18 +0000

    Add validation to form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,38 @@
+import { ChangeEvent, useState } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+interface FormProps {
+  name: string;
+  attendance: 'sim' | 'nao';
+  people: number;
+  kids: string;
+  foodRestrictions: string;
+}
+
 const Form = (): JSX.Element => {
+  const {
+    register,
+    handleSubmit,
+    clearErrors,
+    formState: { errors },
+  } = useForm<FormProps>();
+
+  const [isAttending, setIsAttending] = useState(false);
+
+  const onSubmit: SubmitHandler<FormProps> = (data) => {
+    console.log('isAttending on submit >>> ', isAttending);
+
+    console.log(data);
+  };
+
+  const handleSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    // Tell form if user as selected that they're going
+    setIsAttending(e.target.value === 'sim');
+
+    // Clear attendance error
+    clearErrors('attendance');
+  };
+
   return (
     <div className="mx-auto mt-16 max-w-xl px-6 sm:mt-20 border">
       <h2 className="text-xl font-semibold max-w-3xl mx-auto sm:text-3xl">
@@ -8,7 +42,10 @@ const Form = (): JSX.Element => {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum alias
         sequi natus tempora adipisci eum hic dicta.
       </p>
-      <form action="#" method="POST" className="mx-auto mt-8 max-w-xl sm:mt-12">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="mx-auto mt-8 max-w-xl sm:mt-12"
+      >
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
           <div className="sm:col-span-2">
             <label
@@ -19,13 +56,20 @@ const Form = (): JSX.Element => {
             </label>
             <div className="mt-2.5">
               <input
+                {...register('name', {
+                  required: true,
+                })}
                 type="text"
                 name="name"
                 id="name"
                 autoComplete="given-name"
                 className="form-input"
-                required
               />
+              {errors.name && (
+                <p className="text-red-500 text-xs italic mt-2 ml-1">
+                  Por favor preencha o nome
+                </p>
+              )}
             </div>
           </div>
           <div>
@@ -37,29 +81,42 @@ const Form = (): JSX.Element => {
             </label>
             <div className="relative mt-2.5">
               <select
+                {...register('attendance', {
+                  required: true,
+                })}
                 id="attendance"
                 className="form-input h-10"
                 name="attendance"
-                required
+                onChange={handleSelectChange}
               >
-                <option value={undefined}>Selecione uma opção</option>
+                <option value="">Selecione uma opção</option>
                 <option value="sim">Sim</option>
                 <option value="nao">Não</option>
               </select>
+              {errors.attendance && (
+                <p className="text-red-500 text-xs italic mt-2 ml-1">
+                  Por favor escolha uma opção.
+                </p>
+              )}
             </div>
           </div>
           <div>
             <label
-              htmlFor="number-people"
+              htmlFor="people"
               className="block text-sm font-semibold leading-6 text-primary-700"
             >
               Numero de pessoas
             </label>
             <div className="relative mt-2.5">
               <input
+                {...register('people', {
+                  required: isAttending,
+                  min: 0,
+                  disabled: !isAttending,
+                })}
                 type="number"
-                name="number-people"
-                id="number-people"
+                name="people"
+                id="people"
                 className="form-input"
               />
             </div>
@@ -73,6 +130,9 @@ const Form = (): JSX.Element => {
             </label>
             <div className="mt-2.5">
               <input
+                {...register('kids', {
+                  disabled: !isAttending,
+                })}
                 type="text"
                 name="kids"
                 id="kids"
@@ -91,6 +151,9 @@ const Form = (): JSX.Element => {
             </label>
             <div className="mt-2.5">
               <textarea
+                {...register('foodRestrictions', {
+                  disabled: !isAttending,
+                })}
                 name="food-restrictions"
                 id="food-restrictions"
                 rows={2}
