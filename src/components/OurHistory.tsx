da9b9d2e497ec2079a53ac47904e6d90221ee78a
Gonçalo Ramalho <goncalo_ramalho@hotmail.com>
2024-03-05 16:44:47 +0000

    Add styles for RSVP and improve spacing in our history

--- src/components/OurHistory.tsx
+++ src/components/OurHistory.tsx
@@ -5,9 +5,9 @@ export default function OurHistory() {
   return (
     <div
       id="our_history"
-      className="grid xl:grid-cols-2 xl:grid-flow-col gap-x-8 py-20 md:py-26 relative m-auto min-h-lvh"
+      className="grid xl:grid-cols-2 xl:grid-flow-col gap-x-11 py-20 md:py-26 relative m-auto min-h-lvh"
     >
-      <article className="text-pretty text-lg px-6 md:px-8 xl:pl-20">
+      <article className="text-pretty text-lg px-6 md:px-8 md:pr-0 xl:pl-20">
         <h2 className="heading-2">A nossa história</h2>
         <p className="py-2">
           Como casal do século XXI que somos, as redes sociais apresentaram-nos
--- src/components/RSVP.tsx
+++ src/components/RSVP.tsx
@@ -1,12 +1,20 @@
 import Form from '@components/Form';
+const foto = new URL('../assets/images/rsvp.jpg', import.meta.url).href;
 
 export default function RSVP() {
   return (
     <div
       id="rsvp"
-      className="grid md:grid-flow-col gap-x-8 py-20 md:py-26 px-6 relative md:px-10 m-auto max-w-[90rem]"
+      className="grid lg:grid-cols-2 lg:grid-flow-col gap-x-11 py-20 md:py-26 relative m-auto"
     >
-      <article className="text-pretty">
+      {/* For landscape */}
+      <img src={foto} alt="our history" className="pt-9 min-w-full lg:hidden" />
+      <div
+        className="text-pretty text-lg px-6 md:px-8 xl:pl-20 border bg-cover bg-no-repeat bg-top h-full w-full"
+        style={{ backgroundImage: `url(${foto})` }}
+      ></div>
+
+      <div className="px-6 md:px-8 md:pl-0 xl:pr-20">
         <h2 className="heading-2">RSVP</h2>
         <p className="text-lg max-w-xl sm:text-xl ">
           Queremos muito contar convosco neste nosso dia, esperamos as vossas
@@ -16,8 +24,8 @@ export default function RSVP() {
           </span>
           .
         </p>
-      </article>
-      <Form />
+        <Form />
+      </div>
     </div>
   );
 }
