1198d466f5042d953b3a504807279095d4a8be4c
Gonçalo Ramalho <goncalo_ramalho@hotmail.com>
2024-03-25 12:04:51 +0000

    Improve mobile menu ux - Align close bin with hamburger menu - Center options instead of align left

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -47,7 +47,7 @@ export default function Header() {
         <div className="flex sm:hidden">
           <button
             type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md  text-headings hover:text-action-400 transition-all"
+            className="inline-flex items-center justify-center rounded-md  text-headings hover:text-action-400 transition-all"
             onClick={() => setMobileMenuOpen(true)}
           >
             <span className="sr-only">Open main menu</span>
@@ -76,7 +76,7 @@ export default function Header() {
       >
         <div className="fixed inset-0 z-50" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center justify-between">
+          <div className="flex items-center justify-end">
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-headings"
@@ -86,7 +86,7 @@ export default function Header() {
               <XMarkIcon className="size-6" aria-hidden="true" />
             </button>
           </div>
-          <div className="mt-6 flow-root">
+          <div className="mt-6 flow-root text-center">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
                 {navigation.map((item) => (
